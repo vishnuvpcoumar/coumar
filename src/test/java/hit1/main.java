7b8be391ea10676241d1c259e49c958e85a6bd7e
Training_C2a.05.08 <Training_C2a.05.08@CDC2-D-513RQFR.dir.svc.accenture.com>
2019-06-14 12:46:36 +0530

    hgj

---
+++ src/test/java/hit1/main.java
@@ -0,0 +1,52 @@
+package hit1;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class main {
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+		System.out.println("VP");
+	 
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+		System.out.println("VP");
+	   
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+		System.out.println("VP");
+	    
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+		System.out.println("VP");
+	  
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+		System.out.println("VP");
+	   
+	}
+
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+		System.out.println("VP");
+	   
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+		System.out.println("VP");
+	}
+
+
+
+
+}
