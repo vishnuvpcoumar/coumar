8152f632167f8533af771c8d23f8bbce7d386ad8
Training_C2a.05.08 <Training_C2a.05.08@CDC2-D-513RQFR.dir.svc.accenture.com>
2019-06-14 17:35:25 +0530

    jgjg

--- src/test/java/hit1/run.java
+++ src/test/java/hit1/run.java
@@ -2,9 +2,14 @@ package hit1;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		monochrome=true,
+		plugin= {"pretty","json:src\\test\\java\\hit1/cucumber-reports.json"}
+		)
 public class run {
 
 }
